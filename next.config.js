947d756f2d9dc2a62ff6f214181a85bf1e014857
lordcharles-01 <coadekunle@outlook.com>
2026-05-23 17:57:19 +0100

    Revert "Force unique build ID to bypass Vercel cache"
    This reverts commit 1f859f77be636cdeeb365bfc549f25b175786f78.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  generateBuildId: async () => {
-    return 'build-' + Date.now()
-  },
-};
+const nextConfig = {};
 module.exports = nextConfig;
